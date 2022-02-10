2b6bd5963126aba70dbb2c4558492e08c8a20b45
luckyLukec <luka.lukac@student.um.si>
2022-02-10 12:13:37 +0100

    Improved pop-up notifications

--- examples/show_image_feedback.py
+++ examples/show_image_feedback.py
@@ -1,3 +1,4 @@
+from PyQt5.QtCore import QEasingCurve
 from PyQt5.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
 import sys
 
@@ -42,7 +43,7 @@ class ShowFeedback(QWidget):
         self._feedback = ImageFeedback('../icons/smile.png', width=icon_width, height=icon_height)
         
         time = 3000
-        self._feedback.show(type_of_animation, time)
+        self._feedback.show(type_of_animation, time, QEasingCurve.InQuad)
 
 
 if __name__ == "__main__":
--- examples/show_text_feedback.py
+++ examples/show_text_feedback.py
@@ -16,22 +16,17 @@ class ShowFeedback(QWidget):
         super(ShowFeedback, self).__init__()
         
         layout = QVBoxLayout(self)
-
         self.textbox = QLineEdit('Let\'s attack on the next hill!' + emoji.emojize(':thumbs_up:'))
         layout.addWidget(self.textbox)
-
         self.button1 = QPushButton('Get vertical feedback', self)
         self.button1.clicked.connect(lambda: self.send_feedback(AnimationType.VERTICAL))
         layout.addWidget(self.button1)
-
         self.button2 = QPushButton('Get horizontal feedback', self)
         self.button2.clicked.connect(lambda: self.send_feedback(AnimationType.HORIZONTAL))
         layout.addWidget(self.button2)
-
         self.button3 = QPushButton('Get diagonal feedback', self)
         self.button3.clicked.connect(lambda: self.send_feedback(AnimationType.MAIN_DIAGONAL))
         layout.addWidget(self.button3)
-
         self.button4 = QPushButton('Get antidiagonal feedback', self)
         self.button4.clicked.connect(lambda: self.send_feedback(AnimationType.ANTI_DIAGONAL))
         layout.addWidget(self.button4)
