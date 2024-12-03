d6376c9d789228f16f31c1a6011843e7f3d58d6d
Iztok Fister Jr. <iztok@iztok.dev>
2024-12-03 12:14:15 +0100

    Add three tests

--- pyqt_feedback_flow/__init__.py
+++ pyqt_feedback_flow/__init__.py
@@ -1,1 +1,9 @@
+from pyqt_feedback_flow.feedback import AnimationType, AnimationDirection, TextFeedback, ImageFeedback
+
+__all__ = [
+    AnimationType,
+    AnimationDirection,
+    TextFeedback,
+    ImageFeedback]
+
 __version__ = '0.3.4'
